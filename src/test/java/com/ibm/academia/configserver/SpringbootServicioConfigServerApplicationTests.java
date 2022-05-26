8fa5e70245f5502cb863f568c53f46bf8a52b7cb
Pedro RM <pedro.reyes0284@alumnos.udg.mx>
2022-05-26 17:52:55 -0500

    Config-server v1.0.0

---
+++ src/test/java/com/ibm/academia/configserver/SpringbootServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.academia.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
